54dc5f7baf51943b13cfd4182e2a8461b364255e
Paulo Estrela <dt.paulohenrique@gmail.com>
2025-05-18 21:41:23 -0300

    Implementacao DadosTemporada, DadosEpisodio, Principal

--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -3,7 +3,10 @@ package br.com.alura.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+//JsonIgnoreProperties ignora os elementos que não foram descritos no record
 @JsonIgnoreProperties(ignoreUnknown = true)
+
+//JsonAlias busca o(s) iten(s) tal qual na API, pode-se adicionar mais de um
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") String avaliacao) {
