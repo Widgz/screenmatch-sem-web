20da3bbcf615179cc4837ab7d5de982548a3f90d
Paulo Estrela <dt.paulohenrique@gmail.com>
2025-05-25 15:40:19 -0300

    Implementando estatisticas e map

--- screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -110,5 +110,20 @@ public class Principal {
                                 " Episódio: " + e.getTitulo() +
                                 " Data de lançamento: " + e.getDataLancamento().format(formatador)
                 ));
+
+        //Media das avaliações por temporada
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println(("Quantidade: " + est.getCount()));
     }
 }
